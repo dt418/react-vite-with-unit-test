1e4ab45d1a21d44799519b8f3f7041b0ff83353d
thanhd <thanhdanh@icd-vn.com>
2024-06-28 08:48:01 +0700

    add: add plop file generate template

---
+++ plopfile.js
@@ -0,0 +1,38 @@
+import { pascalCase } from "change-case";
+
+export default function (plop) {
+  // Helper to convert text to PascalCase
+  plop.setHelper("pascalCase", (text) => pascalCase(text));
+  plop.setGenerator("component", {
+    description: "Create a React component with TypeScript support",
+    prompts: [
+      {
+        type: "input",
+        name: "componentName",
+        message: "Component name:",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx",
+        templateFile: "plop-templates/component.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.lazy.tsx",
+        templateFile: "plop-templates/componentLazy.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.stories.tsx",
+        templateFile: "plop-templates/storybook.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.test.tsx",
+        templateFile: "plop-templates/test.hbs",
+      },
+    ],
+  });
+};
