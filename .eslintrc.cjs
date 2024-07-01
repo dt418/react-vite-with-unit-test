7b796e64b28c75c8c7f4611b61be6ad8114d622d
thanhd <danhthanh418@gmail.com>
2024-07-01 18:22:58 +0700

    update: update eslint to work along side prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,11 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'simple-import-sort'],
+  plugins: ['react-refresh', '@typescript-eslint', 'simple-import-sort', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -17,5 +18,6 @@ module.exports = {
     ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'prettier/prettier': 'error',
   },
 };
