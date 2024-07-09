4fc3fd78ead6c6fa51aafa4345a21de2b2910af0
thanhd <thanhdanh@icd-vn.com>
2024-07-09 15:59:40 +0700

    update: update plopfile.js

--- plopfile.js
+++ plopfile.js
@@ -1,40 +1,40 @@
-import { pascalCase } from "change-case";
+import { pascalCase } from 'change-case';
 
 export default function (
   /** @type {import('plop').NodePlopAPI} */
-  plop
+  plop,
 ) {
   // Helper to convert text to PascalCase
-  plop.setHelper("pascalCase", (text) => pascalCase(text));
-  plop.setGenerator("component", {
-    description: "Create a React component with TypeScript support",
+  plop.setHelper('pascalCase', (text) => pascalCase(text));
+  plop.setGenerator('component', {
+    description: 'Create a React component with TypeScript support',
     prompts: [
       {
-        type: "input",
-        name: "componentName",
-        message: "Component name:",
+        type: 'input',
+        name: 'componentName',
+        message: 'Component name:',
       },
     ],
     actions: [
       {
-        type: "add",
-        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx",
-        templateFile: "plop-templates/component.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx',
+        templateFile: 'plop-templates/component.hbs',
       },
       {
-        type: "add",
-        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.lazy.tsx",
-        templateFile: "plop-templates/componentLazy.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.lazy.tsx',
+        templateFile: 'plop-templates/componentLazy.hbs',
       },
       {
-        type: "add",
-        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.stories.tsx",
-        templateFile: "plop-templates/storybook.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.stories.tsx',
+        templateFile: 'plop-templates/storybook.hbs',
       },
       {
-        type: "add",
-        path: "src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.test.tsx",
-        templateFile: "plop-templates/test.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.test.tsx',
+        templateFile: 'plop-templates/test.hbs',
       },
     ],
   });
