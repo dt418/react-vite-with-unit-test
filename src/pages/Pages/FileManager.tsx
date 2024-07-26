ab7c6c99c554e1d0e4ba6cf74124707c3dc656af
thanhd <thanhdanh@icd-vn.com>
2024-07-26 17:02:44 +0700

    add: add svg icon

--- src/pages/Pages/FileManager.tsx
+++ src/pages/Pages/FileManager.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
+import Logo from '@/images/logo/logo.svg?react';
 
 const FileManager = () => {
   return (
@@ -173,48 +174,7 @@ const FileManager = () => {
           </div>
         </div>
       </div>
-      <div className="mt-7.5 grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <div className="xl:p-7.5 flex flex-col gap-2 p-4 sm:flex-row sm:justify-between md:p-6">
-          <div>
-            <h2 className="text-title-md2 font-bold text-black dark:text-white">
-              Activity Chart
-            </h2>
-          </div>
-          <div className="flex items-center">
-            <p className="font-medium uppercase text-black dark:text-white">
-              Short by:
-            </p>
-            <div className="relative z-20 inline-block">
-              <select
-                name="#"
-                id="#"
-                className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 font-medium outline-none"
-              >
-                <option value="" className="dark:bg-boxdark">
-                  Monthly
-                </option>
-                <option value="" className="dark:bg-boxdark">
-                  Weekly
-                </option>
-              </select>
-              <span className="absolute right-1 top-1/2 z-10 -translate-y-1/2">
-                <svg
-                  width={18}
-                  height={18}
-                  viewBox="0 0 18 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8.99995 12.8249C8.8312 12.8249 8.69058 12.7687 8.54995 12.6562L2.0812 6.2999C1.82808 6.04678 1.82808 5.65303 2.0812 5.3999C2.33433 5.14678 2.72808 5.14678 2.9812 5.3999L8.99995 11.278L15.0187 5.34365C15.2718 5.09053 15.6656 5.09053 15.9187 5.34365C16.1718 5.59678 16.1718 5.99053 15.9187 6.24365L9.44995 12.5999C9.30933 12.7405 9.1687 12.8249 8.99995 12.8249Z"
-                    fill="#64748B"
-                  />
-                </svg>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Logo />
     </>
   );
 };
