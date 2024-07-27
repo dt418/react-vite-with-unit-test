744258a36f5a3b44663cb856cd34cff873563baa
thanhd <danhthanh418@gmail.com>
2024-07-27 17:00:09 +0700

    up: update Breadcrumb test and storybook

--- src/components/Breadcrumb/Breadcrumb.test.tsx
+++ src/components/Breadcrumb/Breadcrumb.test.tsx
@@ -3,7 +3,7 @@ import { renderWithRouter, screen } from '@/utils/routerTest';
 import Breadcrumb from './Breadcrumb';
 
 test('renders breadcrumb with correct page name and link', () => {
-  const pageName = 'Dashboard';
+  const pageName = 'Settings';
 
   renderWithRouter(<Breadcrumb pageName={pageName} />);
 
