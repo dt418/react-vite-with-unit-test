32b341abd092904871057490d69c32dbc3937978
thanhd <thanhdanh@icd-vn.com>
2024-08-08 17:07:10 +0700

    add: add minion lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
@@ -6,76 +7,78 @@ import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      include: ['src/images', 'src/assets', 'public'],
-      exclude: 'node_modules/**',
-      png: {
-        quality: 85,
+const _plugins = [
+  ViteImageOptimizer({
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    include: ['src/images', 'src/assets', 'public'],
+    exclude: 'node_modules/**',
+    png: {
+      quality: 85,
+    },
+    jpeg: {
+      quality: 85,
+    },
+    svg: {
+      multipass: true,
+    },
+    webp: {
+      quality: 85,
+    },
+  }),
+  svgr({
+    svgrOptions: {
+      plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+      svgoConfig: {
+        floatPrecision: 2,
       },
-      jpeg: {
-        quality: 85,
-      },
-      svg: {
-        multipass: true,
-      },
-      webp: {
-        quality: 85,
-      },
-    }),
-    svgr({
-      svgrOptions: {
-        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
-        svgoConfig: {
-          floatPrecision: 2,
+    },
+  }),
+  react(),
+  tsconfigPaths(),
+  VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    scope: '/',
+    includeAssets: [
+      'favicon.svg',
+      'favicon.ico',
+      'robots.txt',
+      'apple-touch-icon.png',
+    ],
+    manifest: {
+      name: 'TailAdmin',
+      short_name: 'TailAdmin',
+      description: 'TailAdmin - Tailwind CSS Admin Dashboard Template',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
         },
-      },
-    }),
-    react(),
-    tsconfigPaths(),
-    VitePWA({
-      injectRegister: 'auto',
-      registerType: 'autoUpdate',
-      scope: '/',
-      includeAssets: [
-        'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
       ],
-      manifest: {
-        name: 'TailAdmin',
-        short_name: 'TailAdmin',
-        description: 'TailAdmin - Tailwind CSS Admin Dashboard Template',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+    },
+  }),
+];
+_plugins.unshift(MillionLint.vite());
+export default defineConfig({
+  plugins: _plugins,
   build: {
     chunkSizeWarningLimit: 1600,
   },
