91b9ba0c499b605d71816b6b61bf52b350ca6c31
thanhd <danhthanh418@gmail.com>
2024-08-11 13:24:47 +0700

    add: add vite-plugin-mkcert for https in dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import mkcert from 'vite-plugin-mkcert';
 import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -75,6 +76,7 @@ const _plugins = [
       ],
     },
   }),
+  mkcert(),
 ];
 _plugins.unshift(MillionLint.vite());
 export default defineConfig({
