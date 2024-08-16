74bc7fcce9ac814c8cdaf9ff92d52be899e7c79c
thanhd <thanhdanh@icd-vn.com>
2024-08-16 16:06:46 +0700

    update: update SvgIcon

--- src/components/SvgIcon/SvgIcon.tsx
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -9,6 +9,20 @@ export type SvgIconProps = Omit<SVGProps<SVGSVGElement>, 'width' | 'height'> & {
   size?: string | number;
 };
 
+/**
+ * The SvgIcon component renders an SVG element with the specified icon and props.
+ * The icon is imported dynamically using the useDynamicSVGImport hook.
+ *
+ *  @example
+ * <SvgIcon icon="home" />
+ * <SvgIcon icon="accessibility" size="2em" className="text-primary-500" />
+ *
+ * @param {IconName} icon - The name of the icon to be displayed
+ * @param {string} [viewBox='0 0 15 15'] - The viewBox attribute for the SVG element
+ * @param {string|number} [size='1em'] - The width and height for the SVG element
+ * @param {string} [testId='svgIcon'] - The testId attribute for the SVG element
+ * @param {object} [props] - Additional props to be passed to the SVG element
+ */
 const SvgIcon: React.FC<SvgIconProps> = ({
   icon,
   viewBox = '0 0 15 15',
@@ -19,10 +33,14 @@ const SvgIcon: React.FC<SvgIconProps> = ({
   const Icon = useDynamicSVGImport(icon);
   return Icon ? (
     <Icon
+      // The data-testid attribute is used by the testing library
       data-testid={testId}
+      // The viewBox attribute sets the coordinate system for the SVG element
       viewBox={viewBox}
+      // The width and height attributes set the size of the SVG element
       width={size}
       height={size}
+      // Additional props are passed to the SVG element
       {...props}
     />
   ) : null;
--- src/hooks/useDynamicSVGImport.tsx
+++ src/hooks/useDynamicSVGImport.tsx
@@ -1,5 +1,11 @@
 import React, { SVGProps, useEffect, useState } from 'react';
 
+/**
+ * Dynamically imports an SVG component based on the provided name.
+ *
+ * @param {string} name - The name of the SVG component to import.
+ * @return {React.FC<SVGProps<SVGSVGElement>> | null} The imported SVG component or null if the import fails.
+ */
 export const useDynamicSVGImport = (name: string) => {
   const [SvgComponent, setSvgComponent] = useState<React.FC<
     SVGProps<SVGSVGElement>
