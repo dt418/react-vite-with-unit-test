b4acca7111bb7e33a96e8fe08f86a0ccef3611c8
thanhd <thanhdanh@icd-vn.com>
2024-08-19 14:00:32 +0700

    fix: fix analytic card

--- src/components/AnalyticCard/AnalyticCard.tsx
+++ src/components/AnalyticCard/AnalyticCard.tsx
@@ -1,29 +1,56 @@
+import humanFormat from 'human-format';
 import React from 'react';
 
 import { cn } from '@/utils/cn';
 
 type ChangeDirection = 'up' | 'down';
+type AnalyticUnit = 'currency' | 'percent' | 'number' | 'time';
 export type AnalyticCardProps = {
   title: string;
-  value: string;
-  changeRate: string;
+  value: number;
+  changeRate: number;
   changeDirection: ChangeDirection;
+  unit: AnalyticUnit;
 };
+const timeScale = new humanFormat.Scale({
+  seconds: 1,
+  minutes: 60,
+  hours: 3600,
+  days: 86400,
+  months: 2592000,
+});
 
 const AnalyticCard: React.FC<AnalyticCardProps> = ({
   title,
   value,
   changeRate,
   changeDirection,
+  unit,
 }) => {
+  const valueToValueFormat = (unit: AnalyticUnit, value: number) => {
+    switch (unit) {
+      case 'currency':
+        return value.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD',
+          maximumFractionDigits: 0,
+        });
+      case 'percent':
+        return `${value}%`;
+      case 'number':
+        return humanFormat(value);
+      case 'time':
+        return humanFormat(value, { scale: timeScale, prefix: 'minutes' });
+    }
+  };
   return (
     <div
       data-testid="analyticCard"
-      className="flex items-center justify-center gap-2"
+      className="flex items-center justify-center gap-2 border-b border-stroke pb-5 last:border-none dark:border-strokedark xl:border-b-0 xl:border-r xl:pb-0"
     >
       <div>
         <h4 className="mb-0.5 text-xl font-semibold text-black dark:text-white md:text-title-lg">
-          {value}
+          {valueToValueFormat(unit, value)}
         </h4>
         <p className="text-sm font-medium">{title}</p>
       </div>
@@ -34,6 +61,7 @@ const AnalyticCard: React.FC<AnalyticCardProps> = ({
           viewBox="0 0 19 19"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          className={cn({ 'rotate-180': changeDirection === 'down' })}
         >
           <path
             d="M8.25259 5.87281L4.22834 9.89706L3.16751 8.83623L9.00282 3.00092L14.8381 8.83623L13.7773 9.89705L9.75306 5.87281L9.75306 15.0046L8.25259 15.0046L8.25259 5.87281Z"
@@ -46,7 +74,7 @@ const AnalyticCard: React.FC<AnalyticCardProps> = ({
             'text-meta-8': changeDirection === 'down',
           })}
         >
-          {changeRate}
+          {changeRate}%
         </span>
       </div>
     </div>
--- src/mocks/analytic.ts
+++ src/mocks/analytic.ts
@@ -1,5 +1,7 @@
 import { ApexOptions } from 'apexcharts';
 
+import { AnalyticCardProps } from '@/components/AnalyticCard/AnalyticCard';
+
 export const analyticOptions: ApexOptions = {
   chart: {
     type: 'bar',
@@ -59,5 +61,36 @@ export const analyticSeries: ApexAxisChartSeries = [
       150, 380, 220, 310, 160, 190, 300, 110, 220, 340, 210, 112, 290, 180, 210,
       170, 270, 200, 160, 240, 120, 200, 180, 230, 220, 160, 130, 140, 330, 280,
     ],
+  },
+];
+
+export const analyticList: AnalyticCardProps[] = [
+  {
+    title: 'Unique Visitors',
+    value: 4350,
+    changeRate: 18,
+    changeDirection: 'up',
+    unit: 'currency',
+  },
+  {
+    title: 'Total Pageviews',
+    value: 55899,
+    changeRate: 25,
+    changeDirection: 'up',
+    unit: 'number',
+  },
+  {
+    title: 'Visit Duration',
+    value: 54,
+    changeRate: 5,
+    changeDirection: 'down',
+    unit: 'percent',
+  },
+  {
+    title: 'Bounce Rate',
+    value: 176,
+    changeRate: 12,
+    changeDirection: 'up',
+    unit: 'time',
   },
 ];
