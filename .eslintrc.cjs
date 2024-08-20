c2fb3cff1353b82ff6567d26d2e90c97e0a3f1e9
thanhd <thanhdanh@icd-vn.com>
2024-08-20 12:47:14 +0700

    update: update project config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+require('eslint-plugin-unused-imports');
 const path = require('path');
 
 module.exports = {
@@ -18,25 +19,39 @@ module.exports = {
     '@typescript-eslint',
     'simple-import-sort',
     'prettier',
+    'unused-imports',
   ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
     // to enforce using type for object type definitions, can be type or interface
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'prettier/prettier': ['error'],
-    "tailwindcss/no-custom-classname": ["error", {
-      "whitelist": ['modal', 'modal-container', '', 'error', 'form-datepicker'],
-    }],
+    'tailwindcss/no-custom-classname': [
+      'error',
+      {
+        whitelist: ['modal', 'modal-container', '', 'error', 'form-datepicker'],
+      },
+    ],
   },
   settings: {
     tailwindcss: {
-      config: path.join(__dirname, "./tailwind.config.ts"),
+      config: path.join(__dirname, './tailwind.config.ts'),
     },
   },
 };
